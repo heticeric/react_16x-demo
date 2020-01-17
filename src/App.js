96868a02bd8be47344b2760cfbc411a340ca9c1f
erixtekila <erixtekila@gmail.com>
2020-01-17 14:39:52 +0100

    [new] Insert JS data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,45 @@
 import React from 'react';
 import './App.css';
 
+const students = 
+[
+  {
+    id :1
+    ,name:"eric"
+  }
+  ,{
+    id :2
+    ,name:"antoine"
+  }
+  ,{
+    id :3
+    ,name:"hélène"
+  }
+  ,{
+    id :4
+    ,name:"yves"
+  }
+  ,{
+    id :5
+    ,name:"bastien"
+  }
+  ,{
+    id :6
+    ,name:"namiko"
+  }
+];
+
 function App() {
   return (
     <div className="App">
-      HETIC rocks
+      <ul>
+        {
+          students.map
+          (
+            s => <li key={ s.id }>{ s.name }</li>
+          )
+        }
+      </ul>
     </div>
   );
 }
