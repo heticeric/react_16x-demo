17471ee0333d9acd3099206f652cd271d9a8d6d0
erixtekila <erixtekila@gmail.com>
2020-01-17 15:36:54 +0100

    [new] Add event

--- src/components/Student.jsx
+++ src/components/Student.jsx
@@ -1,3 +1,26 @@
 import React from "react";
 
-export default ( { settings } ) => <li>{ settings.name }</li>
+export default class extends React.Component
+{
+    state =
+    {
+        edited :false
+    }
+
+    render()
+    {
+        const { settings } = this.props;
+        return(
+            <li>
+                <input
+                    type="checkbox"
+                    onChange=
+                    { 
+                        e => this.setState( { edited : e.target.checked } ) 
+                    }
+                />
+                { settings.name }
+            </li>
+        )
+    }
+}
